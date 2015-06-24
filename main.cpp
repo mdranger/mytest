f6b82bbce09d0bb594f9526f611e10054fb70bb4
mdranger <zli@jingtum.com>
2015-06-24 08:54:58 +0800

    Add more modification

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
   printf("This is my test\n");
   printf("Ad another change\n");
   printf("Add git branch mdrange\n");
+  printf("Add git branch mdranger on 06/23/2015\n");
   return 1;
 }
 
